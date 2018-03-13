e6d5986d7f2e9cfe86ee4f453399b341bc0ff58f
dornescustefan <dornescu.stefan@gmail.com>
2018-03-13 18:20:21 +0000

    How to Use Menu adjusments

--- src/sample/MainGUIController.java
+++ src/sample/MainGUIController.java
@@ -11,13 +11,17 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.PopupWindow;
 import javafx.stage.Stage;
-
+import javafx.scene.text.FontWeight;
 import javax.swing.*;
+import javax.swing.text.StyledEditorKit;
 import java.io.File;
 import java.io.IOException;
+
+import static javafx.scene.paint.Color.DARKBLUE;
 
 /**
  * Created by DOS on 8/20/2017.
@@ -91,24 +95,24 @@ public class MainGUIController  {
             stage.setTitle("How to use..");
             final Group rootGroup = new Group();
             final Scene scene = new Scene(rootGroup, 600, 250);
-
             final Text text1 = new Text(
                     25, 25,
-                    "1. Click on a Table button, a table order form will pop up. "+ System.lineSeparator()
+                    "1. Click on a Table button - a table order form will pop up. "+ System.lineSeparator()
                     +System.lineSeparator()+
-                            "2.Choose a pizza from the menu list, choose quantity from drop down list, " +  System.lineSeparator()
-                            +"press Add to Order button and Click on the Menu List" + System.lineSeparator()
+                            "2.Choose a Menu item from the menu list, choose Quantity from drop down list, " +  System.lineSeparator()
+                            +"press Add to order button and Click on the Menu list (Repeat)" + System.lineSeparator()
                     +System.lineSeparator()+
-                            "3.Press Place Order Button, the order will appear in the Kitchen window"+ System.lineSeparator()
+                            "3.Press Place order button, the order will appear in the Kitchen window"+ System.lineSeparator()
                     +System.lineSeparator()+
-                            "4.On the Kitchen window Click on the order in the Orders List and Press the Cook Button, " + System.lineSeparator()
-                            +"then after Click on the order in the Orders List and then on the Ready Button"+ System.lineSeparator()
+                            "4.On the Kitchen window Click on the order in the Orders List and Press the Cook button, " + System.lineSeparator()
+                            +"then after Click on the order in the Orders list and then on the Ready button"+ System.lineSeparator()
                     +System.lineSeparator()+
-                             "5.On the Table order form press the Order Served Button the" + System.lineSeparator()
-                             +"the Pay Order Button "+ System.lineSeparator()
+                             "5.On the Table order form press the Order served button, at the end press" + System.lineSeparator()
+                             +"the Pay order button "+ System.lineSeparator()
             );
-            text1.setFont(Font.font(java.awt.Font.SERIF, 15));
-            rootGroup.getChildren().add(text1);
+
+            text1.setFont(Font.font(java.awt.Font.SERIF, 15 ) );
+            rootGroup.getChildren().add(text1 );
 
             stage.setScene(scene);
             stage.setResizable(false);
